05fc13c038767dc18d83a76744611f66bed712b8
Hilton Geraldo <139666800+Hilton202@users.noreply.github.com>
2025-08-05 20:53:30 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import App_Controle from './component/App_Controle'
+
+function App() {
+  return (
+    <>
+      <App_Controle></App_Controle>
+    </>
+  )
+}
+
+export default App
